28b00529ca85513948792934d4e87dfa22cb8582
Andres Ramirez <ramirezandres1994@gmail.com>
2019-09-19 14:21:50 -0400

    add sys/types to checksum.h

--- libs/checksum.h
+++ libs/checksum.h
@@ -1,5 +1,5 @@
 #define CHECKSUM_HEADER
-#include "header.h"
+#include "sys/types.h"
 
 //Add up checksum, return value will be filled in checksum filed in header
 u_short add_checksum(u_short len_udp, int padding, const u_short *temp);
