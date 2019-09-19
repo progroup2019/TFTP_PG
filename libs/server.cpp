216546746a2908b995f2cca5bb09122f8fbbe9b2
Andres Ramirez <ramirezandres1994@gmail.com>
2019-09-19 14:23:14 -0400

    class server

---
+++ libs/server.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include "server.h"
+server::server() {
+    Header *header = new Header("6969",6969,550,512,150,99,3,5);
+    puerto = 6969;
+    conexion_servidor = socket(AF_INET, SOCK_STREAM, 0); //creamos el socket
+    bzero((char *)&servidor, sizeof(servidor)); //llenamos la estructura de 0's
+    servidor.sin_family = AF_INET; //asignamos a la estructura
+    servidor.sin_port = htons(puerto);
+    servidor.sin_addr.s_addr = INADDR_ANY; //esta macro especifica nuestra dirección
+
+}
+
+void server::init() {
+    if(bind(conexion_servidor, (struct sockaddr *)&servidor, sizeof(servidor)) < 0)
+    {
+        printf("Error al asociar el puerto a la conexion\n");
+        close(conexion_servidor);
+        return;
+    }
+    listen(conexion_servidor, 3); //Estamos a la escucha
+    printf("A la escucha en el puerto %d\n", ntohs(servidor.sin_port));
+    while (1){
+        longc = sizeof(cliente); //Asignamos el tamaño de la estructura a esta variable
+        conexion_cliente = accept(conexion_servidor, (struct sockaddr *)&cliente, &longc); //Esperamos una conexion
+        if(conexion_cliente<0)
+        {
+            printf("Error al aceptar trafico\n");
+            close(conexion_servidor);
+            return;
+        }
+        printf("Conectando con %s:%d\n", inet_ntoa(cliente.sin_addr),htons(cliente.sin_port));
+        if(recv(conexion_cliente, buffer, 100, 0) < 0)
+        { //Comenzamos a recibir datos del cliente
+            //Si recv() recibe 0 el cliente ha cerrado la conexion. Si es menor que 0 ha habido algún error.
+            printf("Error al recibir los datos\n");
+            close(conexion_servidor);
+        }
+        else
+        {
+            printf("%s\n", buffer);
+            bzero((char *)&buffer, sizeof(buffer));
+            send(conexion_cliente, "Recibido\n", 13, 0);
+        }
+    }
+
+    close(conexion_servidor);
+}
---
+++ libs/server.h
@@ -0,0 +1,41 @@
+
+
+#ifndef TFTP_PG_SERVER_H
+#define TFTP_PG_SERVER_H
+#define  MAX_CLIENTS_CONNECTION 10
+
+#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+class server {
+
+public:
+    server();
+
+    void init();
+
+    int conexion_servidor, conexion_cliente, puerto;
+    socklen_t longc;
+    struct sockaddr_in servidor, cliente;
+    char buffer[100];
+
+
+private:
+
+    /*
+     * Declared variables
+     */
+
+};
+
+
+#endif //TFTP_PG_SERVER_H
