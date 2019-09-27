2cfe394e3a22f34e937f61885d4e764090405e63
osvanicolina <osvacolijim@gmail.com>
2019-09-27 06:36:10 -0400

    Fix server.h

--- libs/server.h
+++ libs/server.h
@@ -1,8 +1,10 @@
 
 
+
+
 #ifndef TFTP_PG_SERVER_H
 #define TFTP_PG_SERVER_H
-#define MAX_CLIENTS_CONNECTION 128
+#define  MAX_CLIENTS_CONNECTION 128
 #define BUFFER_SIZE	1024
 #define PORT 6969
 
@@ -18,36 +20,30 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define BUFFER_SIZE	1024
-#define PORT 6969
-#define PACKET_SIZE 512
-
 class server {
 
 public:
-    int socket_server, request;
-    struct sockaddr_in server_addr, client_addr;
-    fd_set read_mask;
-    char buffer[BUFFER_SIZE];
-    socklen_t addrlen;
-
     server();
 
     void init();
 
-    void server_received_file(char *filename);
+    void get_command(char *buffer);
 
-    void server_send_file(char * filename);
+    void server_received_file(char *buffer);
+
+    void server_send_file(char *buffer);
 
 private:
 
     /*
      * Declared variables
      */
-    int conexion_cliente, puerto;
-    
+    int socket_server, conexion_cliente, puerto, request;
+    socklen_t longc;
+    struct sockaddr_in servidor, cliente;
+    char buffer[BUFFER_SIZE];
     Header *header;
-    
+    fd_set read_mask;
 
 };
 
